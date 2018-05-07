6643b8aeab7523bcff75123640a37cd88c16083c
augustoalegon <mac@MacBook-Pro-de-Mac.local>
2018-05-07 15:40:55 -0500

    Style Home Screen with the main view

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -4,16 +4,16 @@ import {
   View
 } from 'react-native'
 import { Provider } from 'react-redux'
-import DebugConfig from '../Config/DebugConfig'
 import { PersistGate } from 'redux-persist/lib/integration/react'
 import {persistStore} from 'redux-persist'
-import createStore from '../Redux'
 
+// Local Libraries
+import DebugConfig from '../Config/DebugConfig'
+import createStore from '../Redux'
+import RootContainer from './RootContainer'
 // Create our store
 const store = createStore()
 const persistor = persistStore(store)
-// Local Libraries
-import RootContainer from './RootContainer'
 
 type Props = {}
 class App extends Component<Props> {
--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react'
 import {
-    View,
-    Text
+  View,
+  Text
 } from 'react-native'
 
+import {ApplicationStyles} from '../Themes'
 
-
-class HomeScreen extends Component{
-    render () {
-        return(
-            <View>
-                <Text>
+class HomeScreen extends Component {
+  render () {
+    return (
+      <View style={ApplicationStyles.screen.mainContainer}>
+        <Text>
                     This is my Home Screen! (:
-                </Text>
-            </View>
-        )
-    }
+        </Text>
+      </View>
+    )
+  }
 }
 
 export default HomeScreen
