690e79e324da48ef1d008c3be277c2763844e2e0
augustoalegon <mac@MacBook-Pro-de-Mac.local>
2018-05-08 11:30:23 -0500

    Add new components to the navigation file

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,9 +1,17 @@
 import {StackNavigator} from 'react-navigation'
 import HomeScreen from '../Containers/HomeScreen'
+import UserProfile from '../Components/UserProfile'
+import CompanyProfile from '../Components/CompanyProfile'
 
 const primaryScreens = StackNavigator({
   Home: {
     screen: HomeScreen
+  },
+  User: {
+    screen: UserProfile
+  },
+  Company: {
+    screen: CompanyProfile
   }
 }, {
   initialRouteName: 'Home',
