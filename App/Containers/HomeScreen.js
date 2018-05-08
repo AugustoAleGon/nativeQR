9069fee351d80f938aa004c4624aadc1b3a76bd3
augustoalegon <mac@MacBook-Pro-de-Mac.local>
2018-05-07 23:12:14 -0500

    Test QR bar code

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -3,16 +3,30 @@ import {
   View,
   Text
 } from 'react-native'
+import QRCodeScanner from 'react-native-qrcode-scanner'
 
+// Styles
 import {ApplicationStyles} from '../Themes'
 
 class HomeScreen extends Component {
+  constructor(props){
+    super(props)
+    this._onSuccessReadingCode = this._onSuccessReadingCode.bind(this)
+  }
+
+  _onSuccessReadingCode (e) {
+    console.log('This is the event: ', e)
+  }
+
   render () {
     return (
       <View style={ApplicationStyles.screen.mainContainer}>
-        <Text>
-                    This is my Home Screen! (:
-        </Text>
+        <View style={ApplicationStyles.screen.header}>
+          <Text>Scan your profile</Text>
+        </View>
+        <QRCodeScanner
+          onRead={(e) => {this._onSuccessReadingCode(e)}}
+          />
       </View>
     )
   }
